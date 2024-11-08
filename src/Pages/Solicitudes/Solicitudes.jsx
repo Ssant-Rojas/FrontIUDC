5efa312b2efb84985348b326a801f64f443d5d12
Kevin Rojas <kevinrojasingsis@gmail.com>
2024-11-07 22:14:30 -0500

    FUTURE: SOLICITUDES-REQUEST VIEWS

---
+++ src/Pages/Solicitudes/Solicitudes.jsx
@@ -0,0 +1,7 @@
+{/* Creación  de las solicitudes que ha generado cada usuario en este
+    PQR que conyeba la salida Endpoint  
+
+    ```bash
+    curl -X GET http://localhost:8080/api/pqrs/{id}
+    ```    
+    */}
